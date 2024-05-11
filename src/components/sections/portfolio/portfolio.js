426e384515ccfaf263194147e53f645c0a3812b7
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2024-05-11 13:12:54 +0200

    adding translation for projects in portfolio

--- src/components/sections/portfolio/portfolio.js
+++ src/components/sections/portfolio/portfolio.js
@@ -1,11 +1,11 @@
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import skills from "../../../data/skills.json";
 import { BoxArrowUpRight } from "react-bootstrap-icons";
 import { Github } from "react-bootstrap-icons";
 import project from "../../../data/project.json";
 
 function Portfolio() {
-  // const { t } = useTranslation();
+  const { t } = useTranslation();
   return (
     <div id="portfolio" className="portfolio">
       <div className="portfolio__skills">
@@ -24,9 +24,9 @@ function Portfolio() {
               alt="portfolio-overview"
             />
             <div className="card-body card-details">
-              <h5 class="card-title">{item.name}</h5>
+              <h5 class="card-title">{t(item.name)}</h5>
               <div className="card-text card-details__describe mb-3">
-                {item.description}
+                {t(item.description)}
               </div>
               <div className="card-details__technologies">
                 {item.technologies.map((item) => (
