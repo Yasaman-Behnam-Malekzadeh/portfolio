a84f1bbe6626b28e8eb85948dedb14803c7fb85d
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2024-05-12 10:14:09 +0200

    add server and change some settings for sending the email

--- src/components/sections/contanctMe.js
+++ src/components/sections/contanctMe.js
@@ -4,11 +4,46 @@ import { House } from "react-bootstrap-icons";
 import { Linkedin } from "react-bootstrap-icons";
 import { Github } from "react-bootstrap-icons";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 import SvgBottom from "../../files/contactMe/svg-bottom.png";
 
 function ContactMe() {
   const { t } = useTranslation("contact me");
+  const [formData, setFormData] = useState({
+    senderEmail: "",
+    emailAddress: "",
+    emailTopic: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData({ [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Email sent successfully");
+      } else {
+        console.error("Failed to send email");
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+  };
+
   return (
     <div id="contact" className="contact-me">
       <din className="contact-me__info ">
@@ -47,30 +82,42 @@ function ContactMe() {
           </a>
         </div>
       </din>
-      {/* <form className="contact-me__form">
+      <form onSubmit={handleSubmit} className="contact-me__form">
         <input
           type="text"
+          name="senderEmail"
+          value={formData.senderName}
+          onChange={handleChange}
           placeholder={t("contactMe.emailNameTitle")}
           className="p-3 form-control mb-3"
         />
         <input
           type="email"
+          name="emailAddress"
+          value={formData.emailAddress}
+          onChange={handleChange}
           placeholder={t("contactMe.emailTitle")}
           className="p-3 form-control mb-3"
         />
         <input
           type="text"
+          name="emailTopic"
+          value={formData.emailTopic}
+          onChange={handleChange}
           placeholder={t("contactMe.emailSubject")}
           className="p-3 form-control mb-3"
         />
         <textarea
+          value={formData.message}
+          name="message"
+          onChange={handleChange}
           placeholder={t("contactMe.emailMessage")}
           className="p-3 form-control mb-3"
         ></textarea>
         <button type="submit" className="btn px-5">
           {t("contactMe.btnSubmit")}
         </button>
-      </form> */}
+      </form>
       <img className="contact-me__svg" src={SvgBottom} alt="wave" />
     </div>
   );
