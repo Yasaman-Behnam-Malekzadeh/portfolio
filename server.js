519bb9a3761c8a9e2477d8740647bfb12118a76c
Yasaman-Behnam-Malekzadeh <malekzadeh.yasi@yahoo.com>
2024-05-13 21:16:11 +0200

    adding contactMe form using expressjs

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 const sendEmail = async (formData) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
+      user: "yasaman.bmk@gmail.com",
+      pass: "wskg rfjd hugx epxe",
     },
   });
 
   let mailOptions = {
     from: formData.senderEmail,
-    to: formData.recipientEmail,
-    subject: formData.subject,
-    text: formData.message,
+    to: "yasaman.bmk@gmail.com",
+    subject: "From Website : " + formData.emailTopic,
+    text: `Sender's Name: " ${formData.senderEmail}" with email : "${formData.emailAddress}"\n\n${formData.message}`,
   };
 
   await transporter.sendMail(mailOptions);
@@ -32,7 +34,6 @@ app.post("/send-email", async (req, res) => {
     res.status(200).json({ message: "Email sent successfully" });
   } catch (error) {
     console.error("Error sending email:", error);
-    res.status(500).json({ error: "Failed to send email" });
   }
 });
 
--- src/components/sections/contanctMe.js
+++ src/components/sections/contanctMe.js
@@ -4,45 +4,54 @@ import { House } from "react-bootstrap-icons";
 import { Linkedin } from "react-bootstrap-icons";
 import { Github } from "react-bootstrap-icons";
 import { useTranslation } from "react-i18next";
-// import { useState } from "react";
+import { useState } from "react";
 
 import SvgBottom from "../../files/contactMe/svg-bottom.png";
 
 function ContactMe() {
   const { t } = useTranslation("translation");
-  // const [formData, setFormData] = useState({
-  //   senderEmail: "",
-  //   emailAddress: "",
-  //   emailTopic: "",
-  //   message: "",
-  // });
+  const [formData, setFormData] = useState({
+    senderEmail: "",
+    emailAddress: "",
+    emailTopic: "",
+    message: "",
+  });
 
-  // const handleChange = (e) => {
-  //   const name = e.target.name;
-  //   const value = e.target.value;
-  //   setFormData({ [name]: value });
-  // };
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Email sent successfully");
+      } else {
+        console.error("Failed to send email");
+      }
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
 
-  //   try {
-  //     const response = await fetch("/send-email", {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(formData),
-  //     });
-  //     if (response.ok) {
-  //       console.log("Email sent successfully");
-  //     } else {
-  //       console.error("Failed to send email");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error sending email:", error);
-  //   }
-  // };
+    setFormData({
+      senderEmail: "",
+      emailAddress: "",
+      emailTopic: "",
+      message: "",
+    });
+  };
 
   return (
     <div id="contact" className="contact-me">
@@ -82,13 +91,13 @@ function ContactMe() {
           </a>
         </div>
       </div>
-      {/* <form onSubmit={handleSubmit} className="contact-me__form">
+      <form onSubmit={handleSubmit} className="contact-me__form">
         <input
           type="text"
           name="senderEmail"
-          value={formData.senderName}
+          value={formData.senderEmail}
           onChange={handleChange}
-          placeholder={t("emailNameTitle")}
+          placeholder={t("contactMe.emailNameTitle")}
           className="p-3 form-control mb-3"
         />
         <input
@@ -96,7 +105,7 @@ function ContactMe() {
           name="emailAddress"
           value={formData.emailAddress}
           onChange={handleChange}
-          placeholder={t("emailTitle")}
+          placeholder={t("contactMe.emailTitle")}
           className="p-3 form-control mb-3"
         />
         <input
@@ -104,20 +113,20 @@ function ContactMe() {
           name="emailTopic"
           value={formData.emailTopic}
           onChange={handleChange}
-          placeholder={t("emailSubject")}
+          placeholder={t("contactMe.emailSubject")}
           className="p-3 form-control mb-3"
         />
         <textarea
           value={formData.message}
           name="message"
           onChange={handleChange}
-          placeholder={t("emailMessage")}
+          placeholder={t("contactMe.emailMessage")}
           className="p-3 form-control mb-3"
         ></textarea>
         <button type="submit" className="btn px-5">
-          {t("btnSubmit")}
+          {t("contactMe.btnSubmit")}
         </button>
-      </form> */}
+      </form>
       <img className="contact-me__svg" src={SvgBottom} alt="wave" />
     </div>
   );
